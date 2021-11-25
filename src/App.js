c224f5d89b823069c3c281a55ecc14e1320e1de7
Yesismiile <yeseniapozovargas@outlook.com>
2021-11-25 21:23:01 +0100

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ReactPlayer from 'react-player';
+import video from './assets/videos/TheGoodDoctor.mp4';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      <ReactPlayer
+      url={'https://www.youtube.com/watch?v=1wEsPDWcRlk'}
+      width='100%'
+      height='100%'
+      controls
+      loop
+      className="react-player"
+      />
     </div>
   );
 }
-
 export default App;
+
+
